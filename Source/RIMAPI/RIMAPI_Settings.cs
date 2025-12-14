a21a4e68011572f6cdb90dd874e3566d10986ee7
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2025-12-14 17:28:58 +0300

    Update to v1.4.0

--- Source/RIMAPI/RIMAPI_Settings.cs
+++ Source/RIMAPI/RIMAPI_Settings.cs
@@ -5,7 +5,7 @@ namespace RIMAPI
 {
     public class RIMAPI_Settings : ModSettings
     {
-        public string version = "1.3.0";
+        public string version = "1.4.0";
         public string apiVersion = "v1";
         public int serverPort = 8765;
         public int refreshIntervalTicks = 300;
