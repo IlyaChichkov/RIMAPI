badca3579297df531dcda96c3e3171b8303d6ab6
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2025-12-27 11:28:56 +0300

    Add order service
    Add order service

---
+++ Source/RIMAPI/RimworldRestApi/BaseControllers/OrderController.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using System.Net;
+using RIMAPI.Core;
+using RIMAPI.Http;
+using RIMAPI.Models;
+using RIMAPI.Services;
+
+namespace RIMAPI.Controllers
+{
+    public class OrderController
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [Post("/api/v1/order/designate/area")]
+        public async Task DesignateArea(HttpListenerContext context)
+        {
+            var body = await context.Request.ReadBodyAsync<DesignateRequestDto>();
+            var result = _orderService.DesignateArea(body);
+            await context.SendJsonResponse(result);
+        }
+    }
+}
