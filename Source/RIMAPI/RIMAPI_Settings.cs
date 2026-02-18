48fc1e2f3c3db59ee77a668f6a36c70a487e9321
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2026-02-18 21:28:03 +0300

    Update to 1.8.1

--- Source/RIMAPI/RIMAPI_Settings.cs
+++ Source/RIMAPI/RIMAPI_Settings.cs
@@ -16,7 +16,7 @@ namespace RIMAPI
         /// Current version of the RIMAPI mod.
         /// <para>Not saved to XML; used for UI display.</para>
         /// </summary>
-        public string version = "1.8.0";
+        public string version = "1.8.1";
 
         /// <summary>
         /// Current supported API protocol version.
--- docs/api.md
+++ docs/api.md
@@ -1,6 +1,6 @@
 ﻿{{ api.get('page_title', '# RimAPI Documentation') }}
 
-**Version**: 1.8.0  
+**Version**: 1.8.1  
 **Endpoints total count**: 145  
 
 {{ api.section.get("Core_API", {}).get('title', '') }}
